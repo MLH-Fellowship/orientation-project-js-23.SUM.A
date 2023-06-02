ee83d36a018ecffb9d961811cbfafe3aa335cf2b
ginohmk <michotall95@gmail.com>
2023-06-03 00:04:33 +0100

    add: Add test for Home page

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+// import AddSkill from "./pages/AddSkill";
+import Home from "./pages/Home";
+import { BrowserRouter } from "react-router-dom";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test("renders add skill page", () => {
+  render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  );
+  const linkElement = screen.getByText("Export");
   expect(linkElement).toBeInTheDocument();
 });
