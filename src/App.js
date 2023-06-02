2438e30ef5e0395457c6dde5c9e3ac0aae4b77bd
ginohmk <michotall95@gmail.com>
2023-06-02 19:40:36 +0100

    add: Add Route to AddSkill page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import AddSkill from "./pages/AddSkill";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/add-skill" element={<AddSkill />} />
     </Routes>
   );
 }
---
+++ src/pages/AddSkill.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddSkill = () => {
+  return <div>Add Skill</div>;
+};
+
+export default AddSkill;
