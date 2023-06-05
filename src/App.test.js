2e92526baa5a343670b1b5bf0364bca029ef6646
ginohmk <michotall95@gmail.com>
2023-06-05 20:32:07 +0100

    feat: used descripe to test Home page

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,14 @@ import { render, screen } from "@testing-library/react";
 import Home from "./pages/Home";
 import { BrowserRouter } from "react-router-dom";
 
-test("renders add skill page", () => {
-  render(
-    <BrowserRouter>
-      <Home />
-    </BrowserRouter>
-  );
-  const linkElement = screen.getByText("Export");
-  expect(linkElement).toBeInTheDocument();
+describe("Home page test", () => {
+  test("renders Home page", () => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+    const linkElement = screen.getByText("Export");
+    expect(linkElement).toBeInTheDocument();
+  });
 });
