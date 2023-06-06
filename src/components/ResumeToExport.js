56cbc9b83cc1eb56589e9f04f74ad7da43f3224c
JAngelGC <ja_gonzalez42@hotmail.com>
2023-06-06 11:52:59 -0600

    Create template for resume

--- src/components/ResumeToExport.js
+++ src/components/ResumeToExport.js
@@ -8,24 +8,68 @@ const ResumeToExport = React.forwardRef((props, ref) => {
   return (
     <div ref={ref} className={classes["page"]}>
       <h1>Resume</h1>
-      <h2>Experience</h2>
-      <ul>
+
+      <ul className={classes["section"]}>
+        <h2 className={classes["title-section"]}>Experience</h2>
+        <hr />
         {ctx.experience.map((exp) => {
-          return <li>{exp}</li>;
+          return (
+            <li key={exp.title} className={classes["subsection"]}>
+              <div className={classes["top-box"]}>
+                <h3 className={classes["title-subsection"]}>{exp.title}</h3>
+                <div className={classes["dates-box"]}>
+                  <p className={classes["date"]}>
+                    {exp.start_date} &nbsp; - &nbsp; {exp.end_date}
+                  </p>
+                </div>
+              </div>
+              <div className={classes["mid-box"]}>
+                <p className={classes["company"]}>{exp.company}</p>
+              </div>
+              <div className={classes["bottom-box"]}>
+                <p className={classes["description"]}>{exp.description}</p>
+              </div>
+            </li>
+          );
         })}
       </ul>
 
-      <h2>Education</h2>
-      <ul>
+      <ul className={classes["section"]}>
+        <h2 className={classes["title-section"]}>Education</h2>
+        <hr />
         {ctx.education.map((edu) => {
-          return <li>{edu}</li>;
+          return (
+            <li key={edu.title} className={classes["subsection"]}>
+              <div className={classes["top-box"]}>
+                <h3 className={classes["title-subsection"]}>{edu.school}</h3>
+                <div className={classes["dates-box"]}>
+                  <p className={classes["date"]}>
+                    {edu.start_date} &nbsp; - &nbsp; {edu.end_date}
+                  </p>
+                </div>
+              </div>
+              <div className={classes["mid-box"]}>
+                <p className={classes["company"]}>
+                  {edu.course}; &nbsp; Grade: {edu.grade}
+                </p>
+              </div>
+            </li>
+          );
         })}
       </ul>
 
-      <h2>Skills</h2>
-      <ul>
+      <ul className={classes["section"]}>
+        <h2 className={classes["title-section"]}>Skills</h2>
+        <hr />
         {ctx.skills.map((ski) => {
-          return <li>{ski}</li>;
+          return (
+            <li
+              key={ski.name}
+              className={`${classes["subsection"]} ${classes["skill"]}`}
+            >
+              {ski.name}: &nbsp; {ski.proficiency}
+            </li>
+          );
         })}
       </ul>
     </div>
--- src/store/resume-context.js
+++ src/store/resume-context.js
@@ -1,20 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ResumeContext = React.createContext({
-  experience: ["Experience 1", "Experience 2", "Experience 3"],
-  education: ["Education 1", "Education 2", "Education 3"],
-  skills: ["Skill 1", "Skill 2", "Skill 3"],
-  dummy: "Angel",
+  experience: [],
+  setExperience: () => {},
+  education: [],
+  setEducation: () => {},
+  skills: [],
+  setSkills: () => {},
 });
 
 export const ResumeContextProvider = (props) => {
+  const [experience, setExperience] = useState([
+    {
+      title: "Software Developer",
+      company: "A Cooler Company",
+      start_date: "October 2022",
+      end_date: "Present",
+      description:
+        "Results-driven software developer with a strong passion for coding and problem-solving. Highly skilled in designing, developing, and maintaining robust and scalable software applications. ",
+      logo: "example-logo.png",
+    },
+    {
+      title: "Software Developer 2",
+      company: "A Cooler Company",
+      start_date: "October 2022",
+      end_date: "Present",
+      description:
+        "Results-driven software developer with a strong passion for coding and problem-solving. Highly skilled in designing, developing, and maintaining robust and scalable software applications. ",
+      logo: "example-logo.png",
+    },
+    {
+      title: "Software Developer 3",
+      company: "A Cooler Company",
+      start_date: "October 2022",
+      end_date: "Present",
+      description:
+        "Results-driven software developer with a strong passion for coding and problem-solving. Highly skilled in designing, developing, and maintaining robust and scalable software applications. ",
+      logo: "example-logo.png",
+    },
+  ]);
+
+  const [education, setEducation] = useState([
+    {
+      course: "B.S. in Computer Science",
+      school: "Monsters University",
+      start_date: "October 2022",
+      end_date: "August 2024",
+      grade: "86%",
+      logo: "example-logo.png",
+    },
+  ]);
+
+  const [skills, setSkills] = useState([
+    {
+      name: "JavaScript",
+      proficiency: "2-4 years",
+      logo: "example-logo.png",
+    },
+    {
+      name: "JavaScript",
+      proficiency: "2-4 years",
+      logo: "example-logo.png",
+    },
+    {
+      name: "JavaScript",
+      proficiency: "2-4 years",
+      logo: "example-logo.png",
+    },
+  ]);
+
   return (
     <ResumeContext.Provider
       value={{
-        experience: ["Experience 1", "Experience 2", "Experience 3"],
-        education: ["Education 1", "Education 2", "Education 3"],
-        skills: ["Skill 1", "Skill 2", "Skill 3"],
-        dummy: "Angel",
+        experience: experience,
+        setExperience: setExperience,
+        education: education,
+        setEducation: setEducation,
+        skills: skills,
+        setSkills: setSkills,
       }}
     >
       {props.children}
