e081f27c631c10e8d3df52a91f41c310aa679e25
Christian Walker <christiandwalker2021@gmail.com>
2023-06-09 02:15:32 -0500

    Added ability to connect to server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Education from "./pages/Education";
+import Education from "./pages/AddEducation";
 import "./App.css";
 
 function App() {
